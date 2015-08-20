bdfd14a99dc64ced2da0bf8b2906c6cfb4c5b3a3
Edgar A. Baeza <ebaeza1992@gmail.com>
2015-08-19 20:08:59 -0500

    selects dependents

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -2,7 +2,14 @@
   var app = angular.module('searcherUbicate', []);
 
   // main controller
-  app.controller('mainController', function CityController($scope, $http) {
-  	//...
+  app.controller('mainController', function mainController($scope, $http) {
+  	// areas
+  	$scope.areas = ['Aeronáutica', 'Educación'];
+
+  	// get data json
+  	$http.get('data-carreras.json').then(function (response) {
+  		$scope.careers = response.data;
+  		// console.log(response.data);
+  	});
   });
 }());
