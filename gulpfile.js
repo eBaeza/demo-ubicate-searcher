31d33e8670fc8ed0342489bdaa4c286c092bd7a5
Edgar A. Baeza <ebaeza1992@gmail.com>
2015-08-21 14:52:48 -0500

    new structure src-dist

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ var notify     = require('gulp-notify');
 
 // only reload when modified html
 gulp.task('html', function() {
-  gulp.src('app/')
+  gulp.src('dist/')
     .pipe(notify({ message : 'modified HTML'}))
     .pipe(livereload());
 });
@@ -17,37 +17,37 @@ gulp.task('html', function() {
 // concat and minify js loaders
 gulp.task('vendorsloaders', function() {
   gulp.src([
-      './app/js/vendors/loadCSS.js',
-      './app/js/vendors/angular-loader.min.js',
-      './app/js/vendors/scripts-loader.min.js'
+      'src/js/vendors/loadCSS.js',
+      'src/js/vendors/angular-loader.min.js',
+      'src/js/vendors/scripts-loader.min.js'
     ])
     .pipe(concat('vendorsloaders.js'))
     .pipe(jsmin())
-    .pipe(gulp.dest('./app/dist/js/'))
+    .pipe(gulp.dest('dist/js/'))
     .pipe(notify({ message : "vendors loaders"}))
     .pipe(livereload());
 });
 
 // concat and minify js scripts
-gulp.task('js', function() {
+gulp.task('appjs', function() {
   gulp.src([
-      './app/app.js',
-      './app/controllers/*.js'
+      'src/app/app.js',
+      'src/app/controllers/*.js'
     ])
     .pipe(concat('appscripts.js'))
     .pipe(jsmin())
-    .pipe(gulp.dest('./app/dist/js/'))
-    .pipe(notify({ message : "app scripts"}))
+    .pipe(gulp.dest('dist/js/'))
+    .pipe(notify({ message : "appjs"}))
     .pipe(livereload());
 });
 
 // compile sass and add prefixes
 gulp.task('sass', function () {
-  gulp.src('./app/sass/styles.s*ss')
+  gulp.src('src/sass/styles.s*ss')
     .pipe(sass({ indentedSyntax: true }).on('error', sass.logError))
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(prefixes('> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1'))
-    .pipe(gulp.dest('./app/dist/css/'))
+    .pipe(gulp.dest('dist/css/'))
     .pipe(notify({ message : "sass autoprefixer"}))
     .pipe(livereload());
 });
@@ -55,14 +55,10 @@ gulp.task('sass', function () {
 // watch modified files
 gulp.task('watch', function () {
   livereload.listen();
-  gulp.watch('app/**/*.html', ['html']);
-  gulp.watch(['app/css/**/*.s*ss'], ['sass']);
-  gulp.watch(['app/js/vendors/**/*.js'], ['vendorsloaders']);
-  gulp.watch([
-      'app/**/*.js', 
-      '!app/dist/**/*.js', 
-      '!app/js/vendors/**/*.js'
-    ], ['js']);
+  gulp.watch('dist/**/*.html', ['html']);
+  gulp.watch(['src/sass/**/.s*ss'], ['sass']);
+  gulp.watch(['src/js/vendors/**/*.js'], ['vendorsloaders']);
+  gulp.watch(['src/app/**/*.js'], ['appjs']);
 });
 
-gulp.task('default', ['sass', 'vendorsloaders', 'js', 'watch']);
+gulp.task('default', ['sass', 'vendorsloaders', 'appjs', 'watch']);
